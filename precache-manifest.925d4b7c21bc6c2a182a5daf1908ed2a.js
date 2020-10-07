a779ed268b8865fcbe198abf3a369a109b4ff473
Marius Galvin <marius.galvin@gmx.de>
2020-10-07 14:08:13 +0200

    Updates

---
+++ precache-manifest.925d4b7c21bc6c2a182a5daf1908ed2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f1f6d1b3ed0d5418ed2ea8d5a7944b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/monster-rolodex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f9ac7a9d82845e8559ce",
+    "url": "/monster-rolodex/static/css/main.9f45fb64.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/monster-rolodex/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ac7a9d82845e8559ce",
+    "url": "/monster-rolodex/static/js/main.75c87468.chunk.js"
+  },
+  {
+    "revision": "2b104c2b836513a4867e",
+    "url": "/monster-rolodex/static/js/runtime-main.e79c8a28.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/monster-rolodex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
